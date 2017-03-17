09e4999fe4a950fd5d218921e64ce06652163d3c
Alexander <sunchos89@mail.ru>
2017-03-17 22:20:16 +0200

    Added text lives and socre

--- main.lua
+++ main.lua
@@ -4,11 +4,15 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local score = 0
+local lives = 5
 
 background = display.newImage("res/img/paralax_space2.png")
 background.x = display.contentWidth
 background.y = display.contentHeight
+
+local scoreText = display.newText("Score: " .. score, display.contentCenterX  - 100 ,  display.contentCenterY - 230 , native.systemFont, 20 )
+local livesText = display.newText("Lives: " .. lives, display.contentCenterX + 100, display.contentCenterY - 230, native.systemFont, 20)
 
 local centerRect = display.newImage("res/img/redShip.png")
 centerRect.x = display.contentWidth / 2
@@ -29,4 +33,5 @@ function centerRect:touch(event)
 	
 		return true
 end
+
 centerRect:addEventListener("touch" , centerRect)
